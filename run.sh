2d6b82001778283e52f9f6969c621d44ea7d4873
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2020-09-26 16:09:51 -0600

    Using python3 to invoke web server.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python -m SimpleHTTPServer 8081 &
+python3 -m http.server 8081 &
 swilgt -g "{'src/loader'}"
 
 # kill the python server before exiting
-ps ax | grep SimpleHTTPServer | head -n1 | awk '{print $1}' | xargs kill
+ps ax | grep 'http.server' | head -n1 | awk '{print $1}' | xargs kill
